d18a8c449c782b6a1f39a4589d46587643f02c73
arielxioxio <71679945+arielxioxio@users.noreply.github.com>
2021-01-09 19:13:01 +0800

    碰撞

---
+++ VR_End_107051071/Assets/scripts/ScoreManger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManger : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+
+  private  void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "沙包") 
+        {
+            AddScore();
+        }
+
+    }
+ private void AddScore()
+    {
+        score += 1;
+        textScore.text = "得分:" + score;
+    }
+
+
+}
