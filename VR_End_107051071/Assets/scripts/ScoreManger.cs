bbf9a168a89321f719663417eacc2820c72e03d5
ariel2234561@gmail.coom <41018526ALre>
2021-01-12 15:20:54 +0800

    VR_107051071(END)

--- VR_End_107051071/Assets/scripts/ScoreManger.cs
+++ VR_End_107051071/Assets/scripts/ScoreManger.cs
@@ -6,19 +6,50 @@ public class ScoreManger : MonoBehaviour
     [Header("分數介面")]
     public Text textScore;
     [Header("分數")]
-    public int score;
+    public static int score;
+    [Header("分數1")]
+    public int score1;
+    [Header("分數2")]
+    public int score2;
+    [Header("進球音效")]
+    public AudioClip soundIn;
 
-  private  void OnTriggerEnter(Collider other)
+    private AudioSource aud;
+
+    private void Awake()
     {
-        if (other.tag == "沙包") 
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "沙包")
         {
             AddScore();
-        }
+            Destroy(GetComponent<ScoreManger>()); 
+            AddScore2();
+            Destroy(GetComponent<ScoreManger>());
 
+
+        }
     }
- private void AddScore()
+    private void OnCollisionEnter(Collider other)
     {
-        score += 1;
+        if(other.tag=="酒")
+        {
+            AddScore2();
+        }
+    }
+
+    private void AddScore()
+    {
+        score +=score1;
+        textScore.text = "得分:" + score;
+        aud.PlayOneShot(soundIn,Random.Range(1.5f,3f));
+    } 
+    private void AddScore2()
+    {
+        score +=score2;
         textScore.text = "得分:" + score;
     }
 
